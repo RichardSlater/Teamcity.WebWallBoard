5cf6787cbf71e7238189c412be634a1fa4d2f785
Richard Slater <github@richard-slater.co.uk>
2011-07-08 21:16:42 +0100

    Added data store project.

---
+++ WallBoard.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Web Wall Board Persistent Data Store")]
+[assembly: AssemblyDescription("Web Wall Board Persistent Data Store")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Web Wall Board Team")]
+[assembly: AssemblyProduct("Web Wall Board")]
+[assembly: AssemblyCopyright("©2011 Web Wall Board Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69a2aefe-674a-4981-9ecc-96d53a7b9ab2")]
+
+// Version information for an assembly consists of the following four values:
+//  - Major Version
+//  - Minor Version 
+//  - Build Number
+//  - Revision
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+
